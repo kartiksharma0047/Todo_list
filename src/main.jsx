ffcf2ff78fde9cd9ad74ed06b0da74ef1d663795
Kartik Sharma <129365236+kartiksharma0047@users.noreply.github.com>
2024-06-16 00:11:49 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-// Change file name only here if function which is import is App. 
-import App from "./todo List/Todo.jsx";
+// // Change the location for Store or else remove it
+// import { store } from './reduxTodoList/store';
+// import { Provider } from 'react-redux'
+
+// Change the location of file
+import App from './todo List/Todo'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <Provider store={store}> */}
+      <App />
+    {/* </Provider> */}
   </React.StrictMode>,
 )
